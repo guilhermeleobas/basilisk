477216f8f5ab17e04c10ce61c0ef63483bb58083
Guilherme Leobas <guilhermeleobas@gmail.com>
2018-11-29 16:11:30 +0100

    Update collect.c

--- Collect/collect.c
+++ Collect/collect.c
@@ -24,12 +24,13 @@ void dump_csv(){
   f = fopen(FILENAME, "w");
   if (f != NULL){
     
-    fprintf(f,"STORE,LOAD,CMP,ADD,SUB,MUL,UDIV,SDIV,UREM,SREM,FADD,FSUB,FMUL,FDIV,FCMP,AND,OR,XOR,BR,CALL,SELECT\n");
-    fprintf(f, "%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu\n", 
+    fprintf(f,"STORE,LOAD,CMP,ADD,SUB,MUL,UDIV,SDIV,UREM,SREM,FADD,FSUB,FMUL,FDIV,FCMP,AND,OR,XOR,BR,CALL,SELECT,SHL,ASHR,LSHR\n");
+    fprintf(f, "%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu,%llu\n", 
               store_inc, load_inc, icmp_inc, add_inc, sub_inc,
               mul_inc, udiv_inc, sdiv_inc, urem_inc, srem_inc,
               fadd_inc, fsub_inc, fmul_inc, fdiv_inc, fcmp_inc,
-              and_inc, or_inc, xor_inc, br_inc, call_inc, select_inc);
+              and_inc, or_inc, xor_inc, br_inc, call_inc, select_inc,
+              shl_inc, ashr_inc, lshr_inc);
 
     /* if (size){ */
     /*   fprintf(f, "%s", array[0].name); */
